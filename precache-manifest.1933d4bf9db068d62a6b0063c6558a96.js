e3873255a70e3c6864837f738939efbe83975f9d
feridum <aleksander.patschek@gmail.com>
2019-07-07 15:16:30 +0200

    Updates

---
+++ precache-manifest.1933d4bf9db068d62a6b0063c6558a96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52439e600088fb1e5d45f276f7a4ccd5",
+    "url": "/mafia-game-master/index.html"
+  },
+  {
+    "revision": "1c1d8496cb626d9d1fb3",
+    "url": "/mafia-game-master/static/js/2.3a66525e.chunk.js"
+  },
+  {
+    "revision": "5baa8b49d7923f4d14d5",
+    "url": "/mafia-game-master/static/js/main.27eee935.chunk.js"
+  },
+  {
+    "revision": "99d804da2b77779c98fd",
+    "url": "/mafia-game-master/static/js/runtime~main.47bd6a44.js"
+  }
+]);
